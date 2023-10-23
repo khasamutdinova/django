b35c1fc9423eb5182bf601e859ae22e15cc5a474
myblog <khasamutdinova_e@iuca.kg>
2023-10-23 11:12:41 +0600

    Last commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+# from .views import ListView, CreateView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # path('', ListView.as_view(), name='post_list'),
+    # path('post/<int:pk>', CreateView.as_view(), name='post_new'),
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render, get_object_or_404, redirect
+# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import get_object_or_404
+# from django.views.generic import ListView, CreateView, UpdateView
+
+
+# class ListView(ListView):
+#     model = Post
+#     template_name = 'blog/post_list.html'
+#     context_object_name = 'posts'
+
+# class CreateView(CreateView):
+#     form_class = PostForm
+#     template_name = 'blog/post_new.html'
+#     context_object_name = 'post'
+#     PermissionRequired = ('post_new')
+
+#     def form_valid(self, form):
+#        post = form.save(commit=False)
+#        post.published_date = timezone.now()
+#        post.save()
+#        return redirect ('post_detail', pk=post.pk)
+    
+# class PostEdit(UpdateView):
+#     model = Post
+#     form_class = PostForm
+#     template_name = 'blog/post/edit.html'
+
+#     def form_valid(self, form):
+#         post = form.save(commit=False)
+#         post.author = self.request.user
+#         post.published = self.request.user
+#         post.save()
+#         return redirect('post_detail', pk=post.pk)
+
+
+
 
 
 def post_list(request):
@@ -12,6 +48,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+# @login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
